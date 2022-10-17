df03354ff4db05f5a55e92b67a6bf84e7bc49056
Brandon Rodriguez <chilebr99@gmail.com>
2022-10-17 11:06:21 -0400

    Added random color gen

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const grid = document.getElementById("grid-container");
 const gridSquares = document.querySelector("div");
-const rainbowBtn = document.getElementsByClassName("rgb")
-const clearBtn = document.getElementsByClassName("clear")
-const eraserBtn = document.getElementsByClassName("erase")
-const shadow = document.getElementsByClassName("shadow")
-
+const rainbowBtn = document.getElementsByClassName("rgb");
+const clearBtn = document.getElementsByClassName("clear");
+const eraserBtn = document.getElementsByClassName("erase");
+const shadow = document.getElementsByClassName("shadow");
 
 // Creates a 16x16 grid on load
 makeGrid = () => {
@@ -15,10 +14,17 @@ makeGrid = () => {
   }
 };
 
-// Removes class and adds black color CSS class
-gridSquares.addEventListener("mouseover", function(event) {
-  event.target.classList.replace("square", "black-square")
-});
-
-
 makeGrid();
+
+// Creates a random RGB color for user
+colorGen = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  gridSquares.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+};
+
+// Removes class and adds black color CSS class
+gridSquares.addEventListener("mouseover", function (event) {
+  event.target.classList.replace("square", "black-square");
+});
