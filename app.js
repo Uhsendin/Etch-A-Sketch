c28737cdaf37351b3d148692bcb312e3811f23a8
Brandon Rodriguez <chilebr99@gmail.com>
2022-10-19 10:39:53 -0400

    Added color change div to black

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+const grid = document.querySelector(".grid-board");
+const div = grid.querySelectorAll("div");
 function makeGrid(size) {
-  let grid = document.querySelector(".grid-board");
-  let div = grid.querySelectorAll("div")
   div.forEach((div) => div.remove());
   grid.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-  for (let i = 0; i < 256; i++) {
-    let div = document.createElement("div");
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.addEventListener("mouseover", changeColor);
     div.style.backgroundColor = "white";
     grid.insertAdjacentElement("beforeend", div);
   }
 }
-makeGrid(16)
+makeGrid(16);
 
 function gridSize(input) {
-  makeGrid(input)
+  makeGrid(input);
+}
+
+function changeColor() {
+  this.style.backgroundColor = "black";
 }
